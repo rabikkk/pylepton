c98cb3f3b27620e81849c281b4bf2f482bb4d043
rabikkk <doganrabia477@gmail.com>
2021-11-30 11:59:28 +0000

    step fonksiyonu eklendi. tek frame ekrana basıldı.

--- capture_image.py
+++ capture_image.py
@@ -4,13 +4,38 @@ from pylepton import Lepton
 import time
 from skimage import data
 from skimage.filters import threshold_multiotsu
+from skimage.metrics import structural_similarity as ssim
 import ctypes
 ffc = ctypes.cdll.LoadLibrary("/home/pi/Documents/pylepton/leptonSDKEmb32PUB/libLepton_SDK.so")
 ffc_counter=0
 dim = (640,480)#new dimensions
 
+
+imageA=np.zeros((64,64,3))
+imageB=np.zeros((64,64,3))
+image=np.zeros((64,64,3))
+x=0
+start_time = time.time() # start time of the loop
+def step():
+	while True:
+		global x,imageA,imageB
+		if x%2==0:
+			imageA=capture()
+		else:
+			imageB=capture()
+		x+=1
+		if x>=2:
+			s = ssim(imageA, imageB)
+			if s==1.0:
+				pass
+			else:
+				break
+	return cv2.cvtColor(imageA,cv2.COLOR_GRAY2RGB)
+				
+		
+
 def capture():
-	global ffc_counter,x
+	global ffc_counter
 	with Lepton() as l:
 		image,_ = l.capture()
 		if ffc_counter==0:
@@ -35,7 +60,5 @@ def capture():
 		
 	
 		ret,image = cv2.threshold(image, thresholds[0], 255, cv2.THRESH_BINARY)
-		image=cv2.cvtColor(image,cv2.COLOR_GRAY2RGB)
-		image = cv2.resize(image, None, fx=64/80, fy=64/60, interpolation = cv2.INTER_CUBIC)
-		
+		image = cv2.resize(image, None, fx=64/80, fy=64/60, interpolation = cv2.INTER_CUBIC)	
 	return image
