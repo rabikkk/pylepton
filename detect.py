b1d84313e7ba232eff951f39f2e4b80b156a25d7
rabikkk <doganrabia477@gmail.com>
2021-11-30 11:58:42 +0000

    hatalı kod düzeltme

--- detect.py
+++ detect.py
@@ -2,19 +2,16 @@ import re
 import cv2
 import tensorflow as tf
 import numpy as np
-# ~ from capture_image import capture
-from compare import capture 
+from capture_image import step
 import time
 from PIL import Image
 from matplotlib import pyplot as plt
 
 def preprocess_image(frame):
   """Preprocess the input image to feed to the TFLite model"""
-  # ~ frame1 = tf.image.convert_image_dtype(frame, tf.uint8)
-  frame1= tf.image.convert_image_dtype(frame,dtype=tf.uint8)
-  # ~ frame = tf.image.decode_png(frame, channels=3, dtype=tf.uint8)  
-  original_image = frame1
-  resized_img = frame1[tf.newaxis, :]
+  frame = tf.image.convert_image_dtype(frame, tf.uint8)
+  original_image = frame
+  resized_img = frame[tf.newaxis, :]
   return resized_img, original_image
 
 def detect_objects(interpreter, image, threshold):
@@ -65,7 +62,12 @@ def get_output_tensor(interpreter, index):
   tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
   return tensor
 
+new='rabia'
+old='rabia'
+
 def run_odt_and_draw_results(frame,labels, interpreter, threshold):
+  global new,old
+
   """Run object detection on the input image and draw the detection results"""
   # Load the input image and preprocess it
   preprocessed_image, original_image = preprocess_image(frame)
@@ -89,36 +91,33 @@ def run_odt_and_draw_results(frame,labels, interpreter, threshold):
     y = ymin - 15 if ymin - 15 > 15 else ymin + 15
 
     # Find the class index of the current object
-    class_id = int(obj['class_id'])
-    print(labels[int(obj['class_id'])])
+    new=int(obj['class_id'])
+    if new!=old:
+      old=new
+      print(labels[int(obj['class_id'])])
     
   original_uint8 = original_image_np.astype(np.uint8)
   return original_uint8
 
-image=np.zeros((64,64,3))
-labels = load_labels()
-# Load the TFLite model
-interpreter = tf.lite.Interpreter(model_path='/home/pi/Documents/pylepton/models/ssdmobnet/model1.tflite')
-interpreter.allocate_tensors()
-threshold=0.7
-start_time = time.time() # start time of the loop
-shape=0
-while True:
-  image=capture()
-  try:
-    # ~ print(image.shape)
-    if (image.shape)!=None:
-      detection_result_image = run_odt_and_draw_results(image,labels, interpreter,threshold)
-    # FPS = 1 / time to process loop
-    # ~ print("FPS: ", 1.0 / (time.time() - start_time))
-    # ~ start_time=time.time() 
-      cv2.imshow('Pi Feed', detection_result_image)
-  
-  except AttributeError:
-    pass
-    
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-			break 
+def main():
+    img1 = cv2.imread('black.bmp')#black image
+    labels = load_labels()
+    # Load the TFLite model
+    interpreter = tf.lite.Interpreter(model_path='/home/pi/Desktop/pylepton/models/ssdmobnet/detect1.tflite')
+    interpreter.allocate_tensors()
+    threshold=0.7
+    while True:
+        frame = step()
+        psnr = cv2.PSNR(img1,frame)
+        if psnr>5.0:
+        # Run inference and draw detection result on the local copy of the original file
+          frame = run_odt_and_draw_results(frame,labels, interpreter,threshold)
+        cv2.imshow('Pi Feed', frame)
 
-cv2.waitKey(0) # waits until a key is pressed
-cv2.destroyAllWindows() # destroys the window showing image		
+        if cv2.waitKey(10) & 0xFF ==ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
+
